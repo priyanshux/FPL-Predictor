ab26c79e449664de0fdc4823d3d6e6bf816842f0
Priyanshu <psingh1002@gmail.com>
2019-12-31 14:44:26 +0530

    importing libraries

---
+++ fpl.py
@@ -0,0 +1,17 @@
+import json
+import csv
+import logging
+import sys
+import requests
+import argparse
+from tqdm import tqdm
+
+MAIN_URL = "https://fantasy.premierleague.com/api/"
+LOGIN_URL = "https://users.premierleague.com/accounts/login/"
+USER_SUMMARY_SUBURL = "element-summary/"
+TEAM_ENTRY_SUBURL = "entry/"
+PLAYERS_INFO_SUBURL = "bootstrap-static/"
+
+'''
+
+'''
